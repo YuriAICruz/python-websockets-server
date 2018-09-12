93e11c10bc7dcbb54f4f54f2087bd4169476e4b6
Yuri Cruz <yuri.cruz@vldomain.vl.com>
2018-09-12 19:35:35 -0300

    new functioning socket server

---
+++ SocketWrapperServer.py
@@ -0,0 +1,25 @@
+class Message:
+    def __init__(self, obj):
+        self.id = obj["id"]
+        self.uid = obj["uid"]
+        self.message = obj["message"]
+
+    def init_from_args(self, id, uid, message):
+        self.id = id
+        self.uid = uid
+        self.message = message
+
+    id = 0
+    uid = ''
+    message = ''
+
+
+class Connection:
+    def __init__(self, uid, name, websocket):
+        self.uid = uid
+        self.name = name
+        self.websocket = websocket
+
+    uid = ''
+    name = ''
+    websocket = ''
--- app.py
+++ app.py
@@ -1,21 +1,37 @@
-# import SocketServer
-#
-# server = SocketServer.Server(5000)
-# server.init()
-
+from SocketWrapperServer import Message
+from SocketWrapperServer import Connection
 from flask import Flask
 from flask_sockets import Sockets
+import json
+from StringIO import StringIO
 
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
+connections = []
+
 
 @sockets.route('/echo')
 def echo_socket(ws):
-    while not ws.closed:
+    while True:
         message = ws.receive()
         ws.send(message)
+
+
+@sockets.route('/gameServer')
+def game_messages_listener(ws):
+    while True:
+        res = ws.receive()
+        msg = Message(json.loads(str(res)))
+
+        if msg.id == 1:
+            print ("add client: " + msg.uid)
+            connections.append(Connection(msg.uid, msg.message, ws))
+
+        json_string = json.dumps(msg.__dict__)
+        print (json_string)
+        send(ws, "ok")
 
 
 @app.route('/')
@@ -23,7 +39,18 @@ def hello():
     return 'Hello World!'
 
 
+def send(ws, msg):
+    ws.send(msg)
+
+
+def send_all(msg):
+    for conn in connections:
+        conn.websocket.send(msg)
+
+
 if __name__ == "__main__":
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
+
     server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
